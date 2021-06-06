5319013b22ffef13b637d10d1821bba99d5f75e1
ostrosco <ostrosco@fastmail.fm>
2021-06-05 20:48:47 -0600

    Changing from using prefix for npm to current_dir in build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,8 @@ use std::process::Command;
 
 fn main() {
     let status = Command::new("npm")
-        .args(&["--prefix", "./frontend", "install", "./frontend"])
+        .current_dir("./frontend")
+        .args(&["install"])
         .status()
         .expect("Failed to run npm install.");
     assert!(status.success());
