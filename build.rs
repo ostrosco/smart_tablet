321e1e8b9b27aaf57123fbd0dfbb4db8f8a90136
ostrosco <ostrosco@fastmail.fm>
2021-06-05 20:37:00 -0600

    Changing from yarn to npm
    - Due to issues with building the application with yarn in Windows
      (namely running webpack from yarn in the build.rs script), we've moved
      to npm as it seems to work properly in Windows.

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
 use std::process::Command;
 
 fn main() {
-    let status = Command::new("yarn")
-        .args(&["--cwd", "./frontend"])
+    let status = Command::new("npm")
+        .args(&["--prefix", "./frontend", "install", "./frontend"])
         .status()
         .unwrap();
     assert!(status.success());
-    let status = Command::new("yarn")
-        .args(&["--cwd", "./frontend", "build"])
+    let status = Command::new("npx")
+        .current_dir("./frontend")
+        .args(&["webpack"])
         .status()
         .unwrap();
     assert!(status.success());
