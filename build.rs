29c3e58e8dcdb57b97fcea9fce30fa8a266fb2b6
ostrosco <ostrosco@fastmail.fm>
2021-06-05 21:16:35 -0600

    Adding conditional steps for Windows versus Linux
    - Due to how Windows spawns batch scripts, we have to run npm and npx
      via `cmd`, otherwise the command will fail with a file not found.
    - Added additional error checking since the builds are _very_ quite if
      something goes wrong.

--- build.rs
+++ build.rs
@@ -1,23 +1,49 @@
 use std::process::Command;
 
 fn main() {
-    let status = match Command::new("npm")
-        .current_dir("./frontend")
-        .arg("install")
-        .status()
-    {
+    // Run `npm install` to install any npm dependencies if they haven't already.
+    let status = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .current_dir("./frontend")
+            .args(&["/C", "npm install"])
+            .status()
+    } else {
+        Command::new("npm")
+            .current_dir("./frontend")
+            .arg("install")
+            .status()
+    };
+    let status = match status {
         Ok(status) => status,
         Err(e) => panic!("Failed to run npm install: {:?}", e),
     };
-    assert!(status.success(), "npm install failed with status: {:?}", status.success());
-    let status = match Command::new("npx")
-        .current_dir("./frontend")
-        .arg("webpack")
-        .status()
-    {
+    assert!(
+        status.success(),
+        "npm install failed with status: {:?}",
+        status.success()
+    );
+
+    // Run `npx webpack` to package up everything nicely for the Rust webserver.
+    let status = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .current_dir("./frontend")
+            .args(&["/C", "npx webpack"])
+            .status()
+    } else {
+        Command::new("npx")
+            .current_dir("./frontend")
+            .arg("webpack")
+            .status()
+    };
+    let status = match status {
         Ok(status) => status,
         Err(e) => panic!("Failed to run npx webpack: {:?}", e),
     };
-    assert!(status.success(), "npx webpack failed with status: {:?}", status.success());
+    assert!(
+        status.success(),
+        "npx webpack failed with status: {:?}",
+        status.success()
+    );
+
     println!("cargo:rerun-if-changed=./frontend/src");
 }
