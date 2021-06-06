94d22488bf283f0425c97d4658c9fe3e5b18b248
ostrosco <ostrosco@fastmail.fm>
2021-06-05 21:02:41 -0600

    Adding additional error logging for the build script

--- build.rs
+++ build.rs
@@ -1,17 +1,23 @@
 use std::process::Command;
 
 fn main() {
-    let status = Command::new("npm")
+    let status = match Command::new("npm")
         .current_dir("./frontend")
-        .args(&["install"])
+        .arg("install")
         .status()
-        .expect("Failed to run npm install.");
-    assert!(status.success());
-    let status = Command::new("npx")
+    {
+        Ok(status) => status,
+        Err(e) => panic!("Failed to run npm install: {:?}", e),
+    };
+    assert!(status.success(), "npm install failed with status: {:?}", status.success());
+    let status = match Command::new("npx")
         .current_dir("./frontend")
-        .args(&["webpack"])
+        .arg("webpack")
         .status()
-        .expect("Failed to run npx webpack.");
-    assert!(status.success());
+    {
+        Ok(status) => status,
+        Err(e) => panic!("Failed to run npx webpack: {:?}", e),
+    };
+    assert!(status.success(), "npx webpack failed with status: {:?}", status.success());
     println!("cargo:rerun-if-changed=./frontend/src");
 }
