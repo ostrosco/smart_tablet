f39b0fdb530203d7f4f97ad4db10fa54c431fe40
ostrosco <ostrosco@fastmail.fm>
2021-06-05 20:40:13 -0600

    Adding error message on panics in build.rs

--- build.rs
+++ build.rs
@@ -4,13 +4,13 @@ fn main() {
     let status = Command::new("npm")
         .args(&["--prefix", "./frontend", "install", "./frontend"])
         .status()
-        .unwrap();
+        .expect("Failed to run npm install.");
     assert!(status.success());
     let status = Command::new("npx")
         .current_dir("./frontend")
         .args(&["webpack"])
         .status()
-        .unwrap();
+        .expect("Failed to run npx webpack.");
     assert!(status.success());
     println!("cargo:rerun-if-changed=./frontend/src");
 }
