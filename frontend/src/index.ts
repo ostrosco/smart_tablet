a909a4eb5a6b9fd7026c9322d97bdaad8bf18d85
John A. Stevens <john@otherskylines.com>
2021-06-13 16:36:05 -0600

    Add location to clock
    Just showing latitude and longitude right now. We can do a reverse
    geocoding lookup to get a more human-readable position once we find the
    right API for that.

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -4,6 +4,21 @@ import './style.css';
 
 dayjs.extend(advancedFormat);
 
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(setPositionInClock);
+} else {
+  console.log("Location not supported.");
+}
+
+function setPositionInClock(position: GeolocationPosition): void {
+  // eventually, we could reverse geocode the lat and long and get a location that's more meaningful to humans
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  const NSStr = lat >= 0 ? 'N' : 'S';
+  const EWStr = long >= 0 ? 'E' : 'W';
+  document.getElementById("clock-location").innerHTML = `(${Math.abs(long).toFixed(2)}&#176;${EWStr}, ${Math.abs(lat).toFixed(2)}&#176;${NSStr})`;
+}
+
 function updateTime() {
   var currentTime = dayjs();
   document.getElementById("clock-time").innerHTML = currentTime.format('h:mm:ss A');
