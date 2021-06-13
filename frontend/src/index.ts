3159d1f288d73a4161be73d93d57587d3fa8d3c9
John A. Stevens <john@otherskylines.com>
2021-06-13 13:13:37 -0600

    Better time format, and make time update dynamically

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,2 +1,14 @@
 import * as dayjs from 'dayjs';
-document.getElementById("date").innerHTML = dayjs().format();
+
+
+function updateTime() {
+  // set date/time in DOM element
+  document.getElementById("date").innerHTML = dayjs().format('h:mm:ss A');
+}
+
+let updateTimeCallback = () => {
+    updateTime();
+    window.requestAnimationFrame(updateTimeCallback);
+}
+
+window.requestAnimationFrame(updateTimeCallback);
