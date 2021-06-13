313bbdf7fba5a1f073e5c9b4aa329adf1263cc3f
John A. Stevens <john@otherskylines.com>
2021-06-13 13:27:05 -0600

    Add support for css in Webpack, and add initial style file
    Followed directions here: https://www.robinwieruch.de/webpack-css

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,4 +1,5 @@
 import * as dayjs from 'dayjs';
+import './style.css';
 
 
 function updateTime() {
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
     ],
   },
   resolve: {
