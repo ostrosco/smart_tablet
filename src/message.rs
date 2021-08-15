de04538c304ce71b8fc5882ec1329281506b1a8b
ostrosco <ostrosco@fastmail.fm>
2021-08-15 11:14:05 -0600

    Updating TS with new message format
    - Since the JSON is tagged, the typescript needed to be updated with the
      new format.
    - Made the tags in the JSON lowercase for consistency.

--- src/message.rs
+++ src/message.rs
@@ -3,6 +3,7 @@ use crate::weather;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
 pub enum UpdateMessage {
     Weather(weather::WeatherReport),
     News(Vec<news::NewsItem>),
