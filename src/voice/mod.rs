3b547e614583bb9fac0a1aadabf3be5b01099c1b
ostrosco <ostrosco@fastmail.fm>
2021-08-30 21:51:10 -0600

    Substantially improving voice recognition
    - Switched from using the model to using streams in Deepspeech. This
      improves processing times substantially and we don't get the same
      slowdown that we used to when processing audio.
    - Brought in webrtc-vad as a library for determining whether or not
      voice is in the audio stream. If if is, we start dumping audio samples
      into the stream for processing.
    - Removed nnnoiseless as a dependency because the noise library doesn't
      support audio streams for our current sample size we get from the mic.
      We can look into aggregating more samples later to allow noise
      removal.

--- src/voice/mod.rs
+++ src/voice/mod.rs
@@ -1,13 +1,11 @@
 use crate::settings::SETTINGS;
-use crossbeam::channel::{Receiver, unbounded};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use nnnoiseless::DenoiseState;
+use crossbeam::channel::{unbounded, Receiver};
 use std::{
-    ops::Neg,
     sync::{Arc, Mutex},
     thread,
-    time::Instant,
 };
+use webrtc_vad::Vad;
 
 // Wrap up the Deepspeech stream so we can send it to our thread.
 struct Model(deepspeech::Model);
@@ -39,7 +37,7 @@ pub fn listen() -> Result<(), Box<dyn std::error::Error>> {
     // documentation for the model.
     let mut supported_configs = device.supported_input_configs()?;
     let config = supported_configs
-        .find(|c| c.channels() == 1 && c.sample_format() == cpal::SampleFormat::F32)
+        .find(|c| c.channels() == 1 && c.sample_format() == cpal::SampleFormat::I16)
         .ok_or("no supported format for microphone")?
         .with_sample_rate(cpal::SampleRate(expected_sample_rate))
         .config();
@@ -51,7 +49,7 @@ pub fn listen() -> Result<(), Box<dyn std::error::Error>> {
     // callback itself.
     let input_stream = device.build_input_stream(
         &config,
-        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        move |data: &[i16], _: &cpal::InputCallbackInfo| {
             tx.send(data.to_vec()).expect("Couldn't send audio data");
         },
         move |err| {
@@ -61,7 +59,7 @@ pub fn listen() -> Result<(), Box<dyn std::error::Error>> {
     input_stream.play()?;
 
     let handle = thread::spawn(move || {
-        process_audio(model, rx, expected_sample_rate);
+        process_audio(model, rx);
     });
     handle.join().unwrap();
     Ok(())
@@ -69,83 +67,63 @@ pub fn listen() -> Result<(), Box<dyn std::error::Error>> {
 
 /// Receive, process, and transcribe received audio from the microphone.
 ///
-fn process_audio(model: Arc<Mutex<Model>>, rx: Receiver<Vec<f32>>, sample_rate: u32) {
-    // These are magic numbers for the recording. These vary depending on the background noise, the
-    // quality of the microphone, and the cadence in which the user speaks. We'll need to figure
-    // out a way to calculate these or at least make them configurable.
-    let silence_amplitude: f32 = 0.1;
-    let silence_time: f32 = 0.4;
-
+fn process_audio(model: Arc<Mutex<Model>>, rx: Receiver<Vec<i16>>) {
     // Since this thread owns the model and will be using it exclusively, we'll just lock the mutex
     // at the beginning and don't bother letting go.
     let model = &mut model.lock().unwrap().0;
 
-    let mut silence_start: Option<std::time::Instant> = None;
-    let mut silence_has_broken = false;
+    let mut stream = None;
 
-    // We're configuring our VecDeque to hold up to ten seconds of audio. We'll manually drain off
-    // samples that exceed this length as we receive samples. This should be enough for most
-    // practical purposes.
-    let secs_of_audio = 10;
-    let queue_length = sample_rate as usize * secs_of_audio;
-    let mut audio_until_pause: Vec<f32> = Vec::with_capacity(queue_length);
+    // We know that Deepspeech only works with 16kHz data so we hard code it here.
+    let mut vad = Vad::new_with_rate_and_mode(
+        webrtc_vad::SampleRate::Rate16kHz,
+        webrtc_vad::VadMode::Quality,
+    );
 
-    rx.iter().for_each(move |mut samps| {
-        let (max_amplitude, min_amplitude) =
-            samps.iter().fold((std::f32::NAN, std::f32::NAN), |m, v| {
-                (m.0.max(*v), m.1.min(*v))
-            });
-        let is_silent =
-            max_amplitude < silence_amplitude && min_amplitude > silence_amplitude.neg();
+    let mut silent_count = 0;
+    let mut speech_found = false;
 
-        if audio_until_pause.len() >= queue_length {
-            audio_until_pause.drain(0..samps.len());
+    rx.iter().for_each(move |samps| {
+        // Since we're dropping the stream after we finish a decode, we need to check each
+        // iteration to see if the stream needs to be re-created.
+        if stream.is_none() {
+            stream = Some(model.create_stream().expect("couldn't create stream"));
         }
-        audio_until_pause.append(&mut samps);
 
-        if is_silent && silence_has_broken {
-            match silence_start {
-                None => silence_start = Some(Instant::now()),
-                Some(silent_time) => {
-                    if silent_time.elapsed().as_secs_f32() >= silence_time {
-                        // Denoise the audio and scale the values to the range of an i16,
-                        // converting to i16 at the same time. Deepspeech can only operate on i16
-                        // values.
-                        let denoised_audio = denoise(&audio_until_pause)
-                            .iter()
-                            .map(|f| (*f * std::i16::MAX as f32) as i16)
-                            .collect::<Vec<i16>>();
+        // Since short words seem to be missed here, we look for _any_ detection in our stream to
+        // decide whether or not this gets placed in the stream for processing.
+        let is_speech = samps
+            .chunks_exact(160)
+            .any(|frame| vad.is_voice_segment(frame) == Ok(true));
 
-                        // For now we just print out the text we receive after de-noising. Later
-                        // on we would expect this to actually take the transcribed audio and
-                        // process it for commands.
-                        println!("{:?}", model.speech_to_text(&denoised_audio));
-                        audio_until_pause.clear();
-                        silence_has_broken = false;
-                    }
+        if is_speech {
+            silent_count = 0;
+            speech_found = true;
+        } else {
+            silent_count += 1;
+        }
+
+        // If speech has been found at all, start dumping data into the stream...even the silence.
+        // This results in a much better parse by Deepspeech.
+        if speech_found {
+            stream.as_mut().unwrap().feed_audio(&samps);
+        }
+
+        // The silent count here is a magic number we're using that we found mostly through
+        // experimentation. At some point we need to figure out a better way to handle this.
+        if silent_count >= 3 && speech_found {
+            let mut stream_taken = stream.take().unwrap();
+            // Due to the rather shocking false positive rate of webrtc-vad we're running into,
+            // We have this as a stopgap. Do an intermediate decode and if Deepspeech says we've
+            // got nothing, just continue collecting data into the stream.
+            if let Ok(val) = stream_taken.intermediate_decode() {
+                if val != String::new() {
+                    println!("{:?}", val);
+                    drop(stream_taken);
+                    speech_found = false;
+                    silent_count = 0;
                 }
             }
-        } else if !is_silent {
-            silence_start = None;
-            silence_has_broken = true;
         }
     });
-}
-
-/// A utility function to denoise the recording using nnnoiseless before we transcribe it.
-fn denoise(audio: &[f32]) -> Vec<f32> {
-    let mut output = Vec::new();
-    let mut buf = [0.0; DenoiseState::FRAME_SIZE];
-    let mut denoise = DenoiseState::new();
-    let mut first = true;
-    audio
-        .chunks_exact(DenoiseState::FRAME_SIZE)
-        .for_each(|chunk| {
-            denoise.process_frame(&mut buf[..], chunk);
-            if !first {
-                output.extend_from_slice(&buf[..])
-            }
-            first = false;
-        });
-    output
 }
