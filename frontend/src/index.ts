85d37552ff3ce203d935b7953472a908f6e1a0e0
John A. Stevens <john@otherskylines.com>
2021-09-24 20:45:03 -0600

    Add websocket support to frontend

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -14,64 +14,63 @@ const globalData = new GlobalData();
 let currentPanel: ContentPanel;
 
 getLocation();
-getWeather();
 
 changeTabTo('clock');
 
-window.requestAnimationFrame(updateTimeCallback);
+window.requestAnimationFrame(frameCallback);
 
 document.getElementById("clockMenuButton").onclick = (e: MouseEvent) => changeTabTo('clock');
 document.getElementById("weatherMenuButton").onclick = (e: MouseEvent) => changeTabTo('weather');
 
-function updateTimeCallback(): void {
+// WebSocket stuff
+
+let socket = new WebSocket("ws://localhost:9000");
+
+socket.onopen = () => {
+  console.log("socket opened");
+};
+
+socket.onmessage = (event) => {
+  console.log("Received message:");
+  console.log(event);
+
+  const messageData = JSON.parse(event.data);
+  console.log(messageData);
+
+  if (messageData.hasOwnProperty('weather')) {
+    const weather = new Weather(messageData.weather);
+    globalData.weather = weather;
+  }
+}
+
+socket.onclose = (event) => {
+  console.log("Socket closed:");
+  console.log(event);
+}
+
+socket.onerror = (error) => {
+  console.log("Socket error:");
+  console.log(error);
+}
+
+function frameCallback(): void {
   currentPanel.animationFrameTick();
-  window.requestAnimationFrame(updateTimeCallback);
+  window.requestAnimationFrame(frameCallback);
 }
 
 function changeTabTo(tab: string) : void {
+  currentPanel?.tearDown();
+
   if (tab === 'clock') {
-    currentPanel?.tearDown();
     currentPanel = new ClockPanel(globalData);
     currentPanel.setUp();
   } else if (tab === 'weather') {
-    currentPanel?.tearDown();
     currentPanel = new WeatherPanel(globalData);
     currentPanel.setUp();
   }
 }
 
 // Helper functions
-
-export async function getWeather(): Promise<void> {
-  const queryString = "http://localhost:8080/weather";
-
-  let response: Response;
-  let responseJson: Weather;
-
-  try {
-    console.log(`Querying ${queryString} ...`);
-    response = await fetch(queryString);
-    console.log('Query completed successfully.');
-  }
-  catch (ex) {
-    console.log("Exception caught querying weather:");
-    console.log(ex);
-    return;
-  }
-
-  try {
-    console.log('Converting query response to json...');
-    responseJson = new Weather((await response.json()).weather);
-    console.log('Converted successfully');
-  }
-  catch (ex) {
-    console.log("Exception caught converting weather response to JSON:");
-    console.log(ex);
-    return;
-  }
-
-  globalData.weather = responseJson;
-}
 
 export async function getLocation(): Promise<void> {
   const queryString = "http://localhost:8080/settings";
